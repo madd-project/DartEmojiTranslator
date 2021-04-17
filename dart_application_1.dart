390079f5553d51001ff734f54e755de4864f1605
Projectcoded <64954854+madd-project@users.noreply.github.com>
2021-04-17 09:07:59 +0530

    Commented a Lot

--- dart_application_1.dart
+++ dart_application_1.dart
@@ -2,30 +2,25 @@ import 'package:emojis/emojis.dart'; // to use Emoji collection
 import 'package:emojis/emoji.dart'; // to use Emoji utilities
 
 void main() {
-  var todoText = "black"; //What You need to Translate
-  List ofTodoText =
-      todoText.split(' '); //Splits texts to Invidually Translate a word
+  var todoText = "I love my Happy Phone"; //What You need to Translate
+  List ofTodoText =todoText.split(' '); //Splits texts to Invidually Translate a word
 
-  int charecters =
-      ofTodoText.length; // int, the number of charectoers or words in todoText
-  List finaltodoEmoji = [];
+  int charecters = ofTodoText.length; // int, the number of charectoers or words in todoText
+  List finaltodoEmoji = []; //Not so Final List is Empty
 
-  for (int i = 0; i < charecters;) {
-    //for loop
-    final rawtodoEmoji =
-        Emoji.byKeyword(ofTodoText[i]); //It gives all emoji as output
-    final todoEmoji = rawtodoEmoji
-        .take(7)
-        .toList(); // this Compresses each set of emoji to Three (limits)
+  for (int i = 0; i < charecters;) { //for loop
+    final rawtodoEmoji = Emoji.byKeyword(ofTodoText[i]); //It gives all emoji as output
+    final todoEmoji = rawtodoEmoji.take(7).toList(); // this Compresses each set of emoji to Seven (limits)
+    
     // print(todoEmoji); // prints those Three Emoji
 
-    finaltodoEmoji.add(todoEmoji);
+    finaltodoEmoji.add(todoEmoji); //Adds Emoji to a Single List
 
-    finaltodoEmoji.shuffle();
+    finaltodoEmoji.shuffle(); // Shuffle the list, You can Remove this one Code.
 
     i++; //Counter increments.
   }
-  var finalTodoEmojiList = finaltodoEmoji.expand((x) => x).toList();
+  var finalTodoEmojiList = finaltodoEmoji.expand((x) => x).toList(); //Now this is Final, Final List.
 
-  print(finalTodoEmojiList);
+ print(finalTodoEmojiList); // Prints that List. of Emojis.
 }
